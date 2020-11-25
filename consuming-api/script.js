2b5f8a1f676852504638389dc7a55462928c5703
Samuel Stalschus <70023098+SamStalschus@users.noreply.github.com>
2020-11-25 20:16:21 -0300

    update
    melhorias na interface e tratamento de erros

--- consuming-api/script.js
+++ consuming-api/script.js
@@ -1,4 +1,4 @@
-$(document).ready(function(){
+$(document).ready(function(){ //executa quando a pag for carregada via jquery
     $("#rua").focusout(function(){ /*Função para quando o usuário  perder o foco*/
         var rua = $("#rua").val();
 
@@ -8,21 +8,36 @@ $(document).ready(function(){
             url : urlStr,
             type : "post",
             dataType :"json",
-            success : function(data){
+            success : function(data){ //se deu tudo certo com a req retorna data
                 console.log(data);
-
-                $("#cep").val(data[0].cep);                              /*preenche o valor com o valor obtido na req*/
-                $("#bairro").val(data[0].bairro);
-                $("#uf").val(data[0].uf);
-                $("#cidade").val(data[0].localidade);
-
+                    try {
+                        var i = 0;
+                        while(data[i].localidade == "Aparecida de Goiânia"){ //para parar de incluir aparecida de Goiânia nas buscas
+                            i++;
+                        }
+                        $("#cep").val(data[i].cep);             //preenche o valor com o valor obtido na req
+                        $("#bairro").val(data[i].bairro);
+                        $("#uf").val(data[i].uf);
+                        $("#cidade").val(data[i].localidade);
+                        $("#rua").val(data[i].logradouro); 
+                    } catch (error) {
+                        alert("Rua não encontrada");
+                        limparDados();
+                    }
             },
             error : function(erro){
                 console.log(erro)
-                alert("Rua inexistente em Goiânia"); //cria um alerta para caso de erro
+                // //cria um alerta para caso de erro
             }
         });
 
     });
 
+    function limparDados(){
+        $("#cep").val(null);        //limpa os dados
+        $("#bairro").val(null);
+        $("#uf").val(null);
+        $("#cidade").val(null);
+        $("#rua").val(null);
+    }
 });
